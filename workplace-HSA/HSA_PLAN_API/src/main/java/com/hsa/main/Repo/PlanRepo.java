d8dc96c6c48d8764037eb768df8ca49d19d3cc52
siddheshm67 <siddheshm67@gmail.com>
2024-07-20 22:35:28 +0530

    deleted old files

--- workplace-HSA/HSA_PLAN_API/src/main/java/com/hsa/main/Repo/PlanRepo.java
+++
@@ -1,9 +0,0 @@
-package com.hsa.main.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hsa.main.entity.PlanEntity;
-
-public interface PlanRepo extends JpaRepository<PlanEntity, Integer> {
-
-}
